f2d1b3d9e37abf8f224c3e0a2e996949b6f45ac4
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-06 09:52:23 +0700

    chore: remove console log

--- packages/components/layout/hooks/useHasSidebar.ts
+++ packages/components/layout/hooks/useHasSidebar.ts
@@ -1,10 +1,10 @@
-import { Children } from 'react'
+import { Children, ReactElement } from 'react'
 import { Sidebar } from '@mochi-ui/sidebar'
 
 export function useHasSidebar(children?: React.ReactNode) {
   const childNodes = Children.toArray(children)
 
   return childNodes.some(
-    (node) => (node as React.ReactElement<any, any>).type === Sidebar,
+    (node) => (node as ReactElement<any, any>).type === Sidebar,
   )
 }
